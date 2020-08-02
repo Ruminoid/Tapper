1337e7ac1669af4eebf214e89f9e00b428b9a7d6
Il Harper <afanyiyu@hotmail.com>
2020-08-03 00:09:28 +0800

    feat: Config Class Update

--- Ruminoid.Tapper.Timer/Config.cs
+++ Ruminoid.Tapper.Timer/Config.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using KeyTap;
+using Newtonsoft.Json;
 using Ruminoid.Common.Helpers;
 
 namespace Ruminoid.Tapper.Timer
 {
     [RuminoidProduct("Tapper.Timer")]
-    public sealed class Config
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class Config : INotifyPropertyChanged
     {
+        #region Current
+
+        public static Config Current { get; set; } = ConfigHelper<Config>.OpenConfig();
+
+        #endregion
+
+        #region KeyTap
+
+        [JsonProperty]
+        private KeyTapManager keyTapManager = new KeyTapManager();
+
+        public KeyTapManager KeyTapManager
+        {
+            get => keyTapManager;
+            set
+            {
+                keyTapManager = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 }
