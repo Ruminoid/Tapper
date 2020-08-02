348b583611663ff5eef301825d5e264cce41eda1
Il Harper <afanyiyu@hotmail.com>
2020-08-03 00:01:33 +0800

    feat: Add Config Class

---
+++ Ruminoid.Tapper.Timer/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ruminoid.Common.Helpers;
+
+namespace Ruminoid.Tapper.Timer
+{
+    [RuminoidProduct("Tapper.Timer")]
+    public sealed class Config
+    {
+    }
+}
