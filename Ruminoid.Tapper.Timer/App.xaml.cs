0f333c6f64d7112706549bba18f53362f56ecbfa
Il Harper <afanyiyu@hotmail.com>
2020-08-03 00:21:34 +0800

    feat: App Startup Logic Update

--- Ruminoid.Tapper.Timer/App.xaml.cs
+++ Ruminoid.Tapper.Timer/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MetroRadiance.UI;
+using Ruminoid.Tapper.Timer.Windows;
 
 namespace Ruminoid.Tapper.Timer
 {
@@ -13,5 +16,44 @@ namespace Ruminoid.Tapper.Timer
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                args.Handled = true;
+                MessageBox.Show(
+                    args.Exception.Message,
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                MessageBox.Show(
+                    ((Exception)args.ExceptionObject)?.Message ?? "Exception",
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            Timer.Properties.Resources.Culture = CultureInfo.CurrentUICulture;
+
+            MainWindow ??= new MainWindow();
+            MainWindow.Show();
+
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeTheme(Theme.Dark));
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeAccent(Accent.Blue));
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
+        }
     }
 }
